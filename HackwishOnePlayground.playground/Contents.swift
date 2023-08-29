afa34809c091e2f567fc7d018cc0a8b9ac91db50
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-08-29 13:36:28 -1000

    Completed 1-8 need to finish part 9

--- HackwishOnePlayground.playground/Contents.swift
+++ HackwishOnePlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//var myFirstvariable = "I've created my firsr variable"
+// var is only used when declaring the variable (container)
+var myFirstVariable: String
+
+myFirstVariable = "I've created my firsr variable"
+//myFirstvariable = "5"
+// QM is key for String, without it becomes intager
+
+var mySecondVariable = " and look I added 2 strings together."
+//Sring concatenation
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
